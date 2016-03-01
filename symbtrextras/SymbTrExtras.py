8bef1cf4b6497333e95cbf9bcb2ff874591300e6
Sertan Senturk <contact@sertansenturk.com>
2016-03-01 15:19:59 +0200

    Added offset and gracenote correction to SymbTrExtras

--- symbtrextras/SymbTrExtras.py
+++ symbtrextras/SymbTrExtras.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from symbtrdataextractor import symbtrreader, extractor
-import pandas as pd 
+import pandas as pd
 import json
 
 class TxtExtras:
@@ -14,11 +14,12 @@ class TxtExtras:
         txt_data = extractor.extract(txt_file)[0]
         mu2_header = symbtrreader.readMu2Header(mu2_file)[0]
 
-        return extractor.merge(txt_data, mu2_header)  # merge
+        return extractor.merge(txt_data, mu2_header)  # merge        
 
     def addUsul2FirstRow(self, txt_file, mu2_file):
         # extract symbtr data
         data = self.getSymbTrData(txt_file, mu2_file)
+
         # get usul variant
         variant = {}
         for vrt in self.usuldict[data['usul']['symbtr_slug']]['variants']:
@@ -44,7 +45,7 @@ class TxtExtras:
                         row[key] = ''
 
                 # make sure that "Sira" column continues consecutively
-                row['Sira'] = index + 2
+                row['Sira'] = index + 2  # 2 instead of 1, since we also add the usul row to the start
 
                 # reassign
                 df.iloc[index] = row
@@ -59,3 +60,56 @@ class TxtExtras:
                 df_usul = df
 
         return df_usul.to_csv(None, sep='\t', index=False, encoding='utf-8')
+
+    def correctOffsetGracenote(self, txt_file, mu2_file):
+        # extract symbtr data
+        data = self.getSymbTrData(txt_file, mu2_file)
+        
+        # get zaman and mertebe from usul variant
+        for usul in self.usuldict.values():
+            for uv in usul['variants']:
+                if uv['mu2_name'] == data['usul']['mu2_name']:
+                    mertebe = uv['mertebe']
+                    zaman = uv['num_pulses']
+                    break
+
+        # correct the offsets and the gracenote durations
+        df = pd.read_csv(txt_file, sep='\t')
+        for index, row in df.iterrows():
+            # recompute the erroneous gracenotes with non-zero duration
+            if row['Kod'] == 8 and row['Ms'] > 0:
+                row['Pay'] = 0
+                row['Payda'] = 0
+                row['Ms'] = 0
+                
+            # recompute zaman and mertebe, if we hit kod 51
+            if row['Kod'] == 51:
+                zaman = row['Pay']
+                mertebe = row['Payda']
+                offset_incr = 0
+            else:
+                # compute offset
+                offset_incr = 0 if row['Payda'] ==0 else float(row['Pay'])/row['Payda']*mertebe/zaman
+            if index == 0:
+                row['Offset'] = offset_incr
+            else:
+                prev_row = df.iloc[index-1]
+                row['Offset'] = offset_incr + prev_row['Offset']
+                        
+            # change null to empty string
+            for key, val in row.iteritems():
+                if pd.isnull(val):
+                    row[key] = ''
+                    
+            # make sure that "Sira" column continues consecutively
+            row['Sira'] = index+1
+            
+            # reassign
+            df.iloc[index] = row
+
+        # warn if the last measure end prematurely, i.e. the last note does not have an integer offset
+        if not (round(row['Offset']*10000)*0.0001).is_integer():
+            print "Ends prematurely!"
+
+        return df.to_csv(None, sep='\t', index=False, encoding='utf-8')
+            
