5a04a9b2b205bdb849878271d47f8ef63ccf35a8
Sertan Senturk <contact@sertansenturk.com>
2016-03-17 22:17:49 +0100

    Updated requirements

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@
 from setuptools import setup
 
 setup(name='symbtrextras',
-      version='1.0',
-      author='Sertan Senturk',
-      author_email='contact AT sertansenturk DOT com',
-      license='agpl 3.0',
-      description='Basic tools to manipulate the SymbTr-scores',
-      url='http://sertansenturk.com',
-      packages=['symbtrextras'],
-      include_package_data=True,
+    version='1.0',
+    author='Sertan Senturk',
+    author_email='contact AT sertansenturk DOT com',
+    license='agpl 3.0',
+    description='Basic tools to manipulate the SymbTr-scores',
+    url='http://sertansenturk.com',
+    packages=['symbtrextras'],
+    include_package_data=True,
+    install_requires=[
+        "pandas",
+    ],
 )
